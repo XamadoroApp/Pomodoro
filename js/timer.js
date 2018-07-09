e6820ba64de8a739a017a4289b7bd5a18157962d
Jeff Surek <jesurek@microsoft.com>
2018-07-09 12:46:20 -0700

    working timer code, non working changing text code

--- js/timer.js
+++ js/timer.js
@@ -2,3 +2,37 @@
 let start = document.getElementById('start-button');
 let stop = document.getElementById('stop-button');
 let timer = document.getElementById('timer');
+
+// Start timer on click
+start.onclick = function() {
+
+    var startMins = 30
+    var countDownDate = (new Date().getTime() + startMins*60000);
+
+    // Update the count down every 1 second
+    var x = setInterval(function() {
+
+        // Get todays date and time
+        var now = new Date().getTime();
+        
+        // Find the distance between now an the count down date
+        var distance = countDownDate - now;
+        
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        
+        // Output the result in an element with id="demo"
+        timer.innerHTML = days + "d " + hours + "h "
+        + minutes + "m " + seconds + "s ";
+        
+        // If the count down is over, write some text 
+        if (distance < 0) {
+            clearInterval(x);
+            timer.innerHTML = "EXPIRED";
+        }
+    }, 1000);
+  
+};
