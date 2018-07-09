0fc1cee6c94de9d9c2ea581aacbb4eba8264e050
Jeff Surek <jesurek@microsoft.com>
2018-07-09 12:38:40 -0700

    Separating out Timer

---
+++ js/timer.js
@@ -0,0 +1,4 @@
+// get the HTML elements by id
+let start = document.getElementById('start-button');
+let stop = document.getElementById('stop-button');
+let timer = document.getElementById('timer');
