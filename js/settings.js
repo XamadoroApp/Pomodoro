837d6bc161533e8d05690de201fae8a9278f257e
Nate Bruneau <nathbr@microsoft.com>
2018-07-11 17:16:55 -0700

    Added Setting feature, testing cookie in a few

---
+++ js/settings.js
@@ -0,0 +1,14 @@
+
+let timesettingbutton = document.getElementById('time-setting-button');
+//let resetbutton = document.getElementById('reset');
+
+// use tabs.insertCSS to change header color on button click
+// red
+
+// back to original
+//resetbutton.onclick = function() { window.alert("Reset onclick session (placeholder)."); };
+
+//start button alert
+timesettingbutton.onclick = function() { window.alert("I have setup the new time for you."); };
+
+//Testing checkin
