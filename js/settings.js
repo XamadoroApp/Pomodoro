2178a469f42130c519c4c183488d9056d8802662
Nate Bruneau <nathbr@microsoft.com>
2018-07-12 17:15:00 -0700

    added default time cookie

--- js/settings.js
+++ js/settings.js
@@ -1,14 +1,66 @@
 
+
+//cookie code
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function checkCookie() {
+    var user = getCookie("username");
+    if (user != "") {
+        alert("Welcome again " + user);
+    } else {
+        user = prompt("Please enter your name:", "");
+        if (user != "" && user != null) {
+            setCookie("username", user, 365);
+        }
+    }
+}
+
+
 let timesettingbutton = document.getElementById('time-setting-button');
-//let resetbutton = document.getElementById('reset');
 
-// use tabs.insertCSS to change header color on button click
-// red
+//set persistent value in textbox
+//var timesetting = getCookie("timesetting");
 
-// back to original
-//resetbutton.onclick = function() { window.alert("Reset onclick session (placeholder)."); };
 
-//start button alert
-timesettingbutton.onclick = function() { window.alert("I have setup the new time for you."); };
+function setHistory(){
+ 
+    //document.getElementById("timesetting-textbox").style="background:red;"; //Change the background color to red.
+    document.getElementById("timesetting-textbox").value=getCookie("timesetting");
+}
 
-//Testing checkin
+setHistory();
+
+timesettingbutton.onclick = function() { 
+    //set Cookie to value of textBox
+    var newValue = document.getElementById("timesetting-textbox").value;
+    //check if Value is number
+    if(isNaN(newValue)){
+        window.alert("Value " + newValue + " is not a number. Please try again."); 
+    } else {
+         //set Cookie to value of textBox
+        setCookie("timesetting",document.getElementById("timesetting-textbox").value,365);
+        var timesetting = getCookie("timesetting");
+        window.alert("I have setup the new time for you." + timesetting + " !"); 
+    }
+    
+};
