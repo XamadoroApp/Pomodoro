2626ea6d9af1acfbf3003986a32db8e7934654aa
Jeff Surek <jesurek@microsoft.com>
2018-07-24 10:10:18 -0700

    Timer code working in background

--- js/background.js
+++ js/background.js
@@ -1,36 +1,41 @@
 // Initialize variables
-var startMins = 1;
+var startMins = 11;
 var minutes = 0;
 var seconds = 0;
+var timerStopped = true;
 
 // create message listener
 browser.runtime.onMessage.addListener(messageReceived);
 
 function messageReceived(msg){
-	if (msg.action == "start"){
-		var countDownDate = (new Date().getTime() + startMins*60000);
-        
-        // Update the count down every 1 second
-        var x = setInterval(function() {
-            
-            // Get todays date and time
-            var now = new Date().getTime();
+	if (msg.action == "start"){ // Get the start message
+		if (timerStopped){ // the timer is stopped, so go!
+			timerStopped = false;
+			var countDownDate = (new Date().getTime() + startMins*60000);
+			
+			// Update the count down every 1 second
+			var x = setInterval(function() {
+				
+				// Get todays date and time
+				var now = new Date().getTime();
 
-            // Find the distance between now an the count down date
-            var distance = countDownDate - now;
+				// Find the distance between now an the count down date
+				var distance = countDownDate - now;
 
-            // Time calculations for days, hours, minutes and seconds
-            //var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-            //var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-            minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-            seconds = Math.floor((distance % (1000 * 60)) / 1000);
-			browser.runtime.sendMessage({minUpdate: minutes,secUpdate: seconds}, function(response){});            
-            
-            // If the count down is over, write some text 
-            if (distance < 0) {
-                clearInterval(x);
-                window.alert("Timer is complete. Take a 5 min break.");
-            }
-        }, 1000);
+				// Time calculations for days, hours, minutes and seconds
+				//var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+				//var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+				minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+				seconds = Math.floor((distance % (1000 * 60)) / 1000);
+				browser.runtime.sendMessage({minUpdate: minutes,secUpdate: seconds}, function(response){});            
+				
+				// If the count down is over, write some text 
+				if (distance < 0) {
+					clearInterval(x);
+					timerStopped = true;
+					window.alert("Timer is complete. Take a 5 min break.");
+				}
+			}, 1000);
+		}
 	}
 }
--- js/timer.js
+++ js/timer.js
@@ -3,27 +3,30 @@ let start = document.getElementById('start-button');
 let minutetext = document.getElementById('minutes');
 let secondtext = document.getElementById('seconds');
 
-// Set timer status
-var timerStopped = true;
-
-// Send the start message on start click
+// Send the start timer message on start click
 start.onclick = function(){
-	if (timerStopped){
-		// timer started
-		timerStopped = false;
-		// send start message
 		browser.runtime.sendMessage(
 			{action: "start"}, function(response){
 
 		});
-	}
 }
 
-// Create message listener
+// Create message listener to get incoming timer update messages
 browser.runtime.onMessage.addListener(messageReceived);
 
 // Receive updates from timer
 function messageReceived(msg){
-	minutetext.innerHTML = msg.minUpdate + ":";
-	secondtext.innerHTML = msg.secUpdate;
+	// Give minutes a leading 0
+	if (msg.minUpdate < 10) {
+		minutetext.innerHTML = "0" + msg.minUpdate + ":";
+	} else {
+		minutetext.innerHTML = msg.minUpdate + ":";
+	}
+	// Give seconds a leading 0
+	if (msg.secUpdate < 10) {
+		secondtext.innerHTML = "0" + msg.secUpdate;
+	} else {
+		secondtext.innerHTML = msg.secUpdate;
+	}
+
 }
